addf4f2e31f6cc8a3b2af3545574c7257505b454
UCM2 RC13 Maintenance <ucm2-rc13-maintenance@local.invalid>
2026-09-22 19:42:11 +0800

    [siheng] preserve handover artifacts

---
+++ artifacts/handover-20260922-r1/output/DefineID.h
@@ -0,0 +1,229 @@
+//创建画面startup的命令索引ID
+#define  _SCREEN_STARTUP                                                       0
+
+//创建画面home的命令索引ID
+#define  _SCREEN_HOME                                                          1
+
+//创建画面about的命令索引ID
+#define  _SCREEN_ABOUT                                                         2
+
+//创建画面home_signal_error的命令索引ID
+#define  _SCREEN_HOME_SIGNAL_ERROR                                             3
+
+//创建画面about_signal_error的命令索引ID
+#define  _SCREEN_ABOUT_SIGNAL_ERROR                                            4
+
+//创建画面mode_select的命令索引ID
+#define  _SCREEN_MODE_SELECT                                                   5
+
+//创建画面maintenance的命令索引ID
+#define  _SCREEN_MAINTENANCE                                                   6
+
+//创建画面device_model_select的命令索引ID
+#define  _SCREEN_DEVICE_MODEL_SELECT                                           7
+
+#define  _TXT_DIS__STARTUP_STARTUP_PROGRESS_PERCENT                            2
+
+#define  _PROGRESS_STARTUP_PROGRESS1                                           1
+
+#define  _TXT_DIS__HOME_HOME_FORCE_VALUE                                       2
+
+#define  _TXT_DIS__HOME_HOME_FORCE_UNIT                                       13
+
+#define  _TXT_DIS__HOME_HOME_STATUS                                            1
+
+#define  _TXT_DIS__HOME_HOME_DEVIATION                                         3
+
+#define  _TXT_DIS__HOME_HOME_DISTRIBUTION                                      4
+
+#define  _TXT_DIS__HOME_HOME_ROD_1                                             5
+
+#define  _TXT_DIS__HOME_HOME_ROD_1_UNIT                                        9
+
+#define  _TXT_DIS__HOME_HOME_ROD_2                                             6
+
+#define  _TXT_DIS__HOME_HOME_ROD_2_UNIT                                       10
+
+#define  _TXT_DIS__HOME_HOME_ROD_3                                             7
+
+#define  _TXT_DIS__HOME_HOME_ROD_3_UNIT                                       11
+
+#define  _TXT_DIS__HOME_HOME_ROD_4                                             8
+
+#define  _TXT_DIS__HOME_HOME_ROD_4_UNIT                                       12
+
+#define  _TXT_DIS__HOME_HOME_PRIMARY_LABEL                                    14
+
+#define  _TXT_DIS__ABOUT_ABOUT_STATUS                                          1
+
+#define  _TXT_DIS__ABOUT_ABOUT_SERIAL                                          4
+
+#define  _TXT_DIS__ABOUT_ABOUT_HARDWARE_VERSION                                6
+
+#define  _TXT_DIS__ABOUT_ABOUT_SOFTWARE_VERSION                                8
+
+#define  _TXT_DIS__ABOUT_ABOUT_MEMORY                                          7
+
+#define  _TXT_DIS__ABOUT_ABOUT_STORAGE                                         9
+
+#define  _TXT_DIS__ABOUT_ABOUT_SERIAL1                                         2
+
+#define  _TXT_DIS__ABOUT_ABOUT_MEMORY1                                         5
+
+#define  _TXT_DIS__ABOUT_ABOUT_MEMORY2                                         3
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_FORCE_VALUE             2
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_FORCE_UNIT             13
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_STATUS                  1
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ERROR_MESSAGE           0
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_DEVIATION               3
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_DISTRIBUTION            4
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ROD_1                   5
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ROD_1_UNIT              9
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ROD_2                   6
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ROD_2_UNIT             10
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ROD_3                   7
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ROD_3_UNIT             11
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ROD_4                   8
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_ROD_4_UNIT             12
+
+#define  _TXT_DIS__HOME_SIGNAL_ERROR_HOME_SIGNAL_ERROR_PRIMARY_LABEL          14
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_STATUS                             1
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_ERROR_MESSAGE                      0
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_SERIAL                             4
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_HARDWARE_VERSION                   6
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_SOFTWARE_VERSION                   8
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_MEMORY                             7
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_STORAGE                            9
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_SERIAL1                            2
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_MEMORY1                            5
+
+#define  _TXT_DIS__ABOUT_SIGNAL_ERROR_ABOUT_MEMORY2                            3
+
+#define  _ANIMATION_MODE_SELECT_ANIM20                                        20
+
+#define  _ANIMATION_MODE_SELECT_ANIM21                                         1
+
+//画面mode_select中动画控件anim23使用的图片
+#define  _IMG_MODE_SELECT_ANIM23                                               7
+
+#define  _ANIMATION_MODE_SELECT_ANIM23                                        23
+
+//画面mode_select中动画控件anim22使用的图片
+#define  _IMG_MODE_SELECT_ANIM22                                               7
+
+#define  _ANIMATION_MODE_SELECT_ANIM22                                         2
+
+#define  _TXT_DIS__MODE_SELECT_MODE_COUNTDOWN                                  5
+
+#define  _TXT_DIS__MODE_SELECT_MODE_AUTO_LABEL1                                3
+
+#define  _TXT_DIS__MODE_SELECT_MODE_MANUAL_LABEL1                              4
+
+//画面maintenance的背景图片
+#define  _IMG_MAINTENANCE                                                      5
+
+#define  _TXT_DIS__MAINTENANCE_MAINT_TITLE                                    10
+
+#define  _TXT_DIS__MAINTENANCE_MAINT_STATUS                                   11
+
+#define  _ANIMATION_MAINTENANCE_ICON1                                          1
+
+//画面maintenance中动画控件Icon5使用的图片
+#define  _IMG_MAINTENANCE_ICON5                                                8
+
+#define  _ANIMATION_MAINTENANCE_ICON5                                          2
+
+//画面maintenance中动画控件Icon9使用的图片
+#define  _IMG_MAINTENANCE_ICON9                                                8
+
+#define  _ANIMATION_MAINTENANCE_ICON9                                          3
+
+#define  _TXT_DIS__MAINTENANCE_MAINT_ITEM_LOG1                                 4
+
+#define  _TXT_DIS__MAINTENANCE_MAINT_ITEM_LOG2                                 5
+
+#define  _TXT_DIS__MAINTENANCE_MAINT_ITEM_LOG3                                 6
+
+#define  _ANIMATION_MAINTENANCE_LOGPOPUP                                     100
+
+#define  _TXT_DIS__MAINTENANCE_LOG_EXPORT_STATE                              101
+
+#define  _TXT_DIS__MAINTENANCE_LOG_EXPORT_PROGRESS                           102
+
+#define  _PROGRESS_MAINTENANCE_PROGRESS1                                     103
+
+//画面device_model_select的背景图片
+#define  _IMG_DEVICE_MODEL_SELECT                                              5
+
+#define  _ANIMATION_DEVICE_MODEL_SELECT_MODEL_SELECTOR_1                      20
+
+//画面device_model_select中动画控件model_selector_2使用的图片
+#define  _IMG_DEVICE_MODEL_SELECT_MODEL_SELECTOR_2                            10
+
+#define  _ANIMATION_DEVICE_MODEL_SELECT_MODEL_SELECTOR_2                      21
+
+//画面device_model_select中动画控件model_selector_3使用的图片
+#define  _IMG_DEVICE_MODEL_SELECT_MODEL_SELECTOR_3                            10
+
+#define  _ANIMATION_DEVICE_MODEL_SELECT_MODEL_SELECTOR_3                      22
+
+//画面device_model_select中动画控件model_selector_4使用的图片
+#define  _IMG_DEVICE_MODEL_SELECT_MODEL_SELECTOR_4                            10
+
+#define  _ANIMATION_DEVICE_MODEL_SELECT_MODEL_SELECTOR_4                      23
+
+//画面device_model_select中动画控件model_selector_5使用的图片
+#define  _IMG_DEVICE_MODEL_SELECT_MODEL_SELECTOR_5                            10
+
+#define  _ANIMATION_DEVICE_MODEL_SELECT_MODEL_SELECTOR_5                      24
+
+//画面device_model_select中动画控件model_selector_6使用的图片
+#define  _IMG_DEVICE_MODEL_SELECT_MODEL_SELECTOR_6                            10
+
+#define  _ANIMATION_DEVICE_MODEL_SELECT_MODEL_SELECTOR_6                      25
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_TITLE                            10
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_STATUS                            1
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_ROW_1                             2
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_ROW_2                             3
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_ROW_3                             4
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_ROW_4                             5
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_ROW_5                             6
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_ROW_6                             7
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_POSITION                          8
+
+#define  _TXT_DIS__DEVICE_MODEL_SELECT_MODEL_CONFIRM_LABEL                     9
+
+#define  _PROGRESS_DEVICE_MODEL_SELECT_MODEL_SCROLL                           30
+
